6706c47f9c01d7f42fc4874ee111bb1efd9053d5
Md Hasan <mdhasan8064@gmail.com>
2022-11-08 10:12:01 +0600

    setup besic server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Assainment server is running')
+})
+
+
+const uri = `mongodb+srv://${process.env.SECRET_USER_ID}:${process.env.SECRET_PASS}@cluster0.di4ojvf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+app.listen(port, () => {
+    console.log('assainment 11 server on ', port)
+})
