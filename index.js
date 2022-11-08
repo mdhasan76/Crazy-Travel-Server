fd77ef902154c050ca6f02d22db33303bf3bf5da
Md Hasan <mdhasan8064@gmail.com>
2022-11-08 22:39:14 +0600

    services data created

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.SECRET_USER_ID}:${process.env.SECRET_PASS}@cluster0.di4ojvf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+const run = async () => {
+    const serviceCollection = client.db("travel").collection('services')
+
+    app.get('/services', async (req, res) => {
+        const query = {};
+        const services = serviceCollection.find(query);
+        const result = await services.limit(3).toArray();
+        res.send(result)
+    })
+}
+
+run().catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log('assainment 11 server on ', port)
