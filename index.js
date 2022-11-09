ada95d99584bfd236a124d595c2ed26e1825f697
Md Hasan <mdhasan8064@gmail.com>
2022-11-09 15:52:10 +0600

    create dtails api & ready for post

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -27,13 +27,25 @@ const run = async () => {
         res.send(result)
     })
 
-
     //service page data
     app.get('/services', async (req, res) => {
         const query = {}
         const services = serviceCollection.find(query);
         const result = await services.toArray();
         res.send(result)
+    })
+
+    // Service Dtails page
+    app.get('/service/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service)
+    })
+
+    //review post
+    app.post('/review', async (req, res) => {
+
     })
 }
 
